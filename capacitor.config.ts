629e6958439994f49b65a9d6707876c2a745be8a
Nur Ridho A <abdurrahmansyah234@gmail.com>
2022-08-05 21:12:41 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Dompetku',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
